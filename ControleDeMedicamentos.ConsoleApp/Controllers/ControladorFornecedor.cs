34dcab52cf7c20c13e4e68b2c036ae20e278492b
gsvsantos <gustavocnsantos02@gmail.com>
2025-05-12 16:36:55 -0300

    refactor: Implementado Controller e Views do `ModuloFornecedor`.

---
+++ ControleDeMedicamentos.ConsoleApp/Controllers/ControladorFornecedor.cs
@@ -0,0 +1,109 @@
+﻿using ControleDeMedicamentos.ConsoleApp.Compartilhado;
+using ControleDeMedicamentos.ConsoleApp.ModuloFornecedor;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeMedicamentos.ConsoleApp.Controllers;
+
+[Route("fornecedor")]
+public class ControladorFornecedor : Controller
+{
+    public IActionResult Menu()
+    {
+        return View("Menu");
+    }
+
+    [HttpGet("cadastrar")]
+    public IActionResult FormularioCadastrar()
+    {
+        return View("Cadastrar");
+    }
+
+    [HttpPost("cadastrar")]
+    public IActionResult Cadastrar(
+        [FromForm] string nome,
+        [FromForm] string telefone,
+        [FromForm] string cnpj)
+    {
+        ContextoDados contexto = new ContextoDados(true);
+        IRepositorioFornecedor repositorioFornecedor = new RepositorioFornecedorEmArquivo(contexto);
+
+        Fornecedor fornecedor = new Fornecedor(nome, telefone, cnpj);
+
+        repositorioFornecedor.CadastrarRegistro(fornecedor);
+
+        ViewBag.Contexto = "Fornecedores";
+        ViewBag.Tipo = "fornecedor";
+        ViewBag.Mensagem = $"O registro \"{fornecedor.Nome}\" foi cadastrado com sucesso!";
+
+        return View("Notificacao");
+    }
+
+    [HttpGet("visualizar")]
+    public IActionResult VisualizarCadastros()
+    {
+        ContextoDados contexto = new ContextoDados(true);
+        IRepositorioFornecedor repositorioFornecedor = new RepositorioFornecedorEmArquivo(contexto);
+
+        ViewBag.Fornecedores = repositorioFornecedor.SelecionarRegistros();
+
+        return View("Visualizar");
+    }
+
+    [HttpGet("editar/{id:int}")]
+    public IActionResult FormularioEditar([FromRoute] int id) // exemplo com [FromRoute], nao precisa pois refere ao id da rota "{excluir/id:int}"
+    {
+        ContextoDados contexto = new ContextoDados(true);
+        IRepositorioFornecedor repositorioFornecedor = new RepositorioFornecedorEmArquivo(contexto);
+
+        ViewBag.Fornecedor = repositorioFornecedor.SelecionarRegistroPorId(id);
+
+        return View("Editar");
+    }
+
+    [HttpPost("editar/{id:int}")]
+    public IActionResult Editar(
+        [FromRoute] int id,  // exemplo com [FromRoute], nao precisa pois refere ao id da rota "{excluir/id:int}"
+        [FromForm] string nome,
+        [FromForm] string telefone,
+        [FromForm] string cnpj)
+    {
+        ContextoDados contexto = new ContextoDados(true);
+        IRepositorioFornecedor repositorioFornecedor = new RepositorioFornecedorEmArquivo(contexto);
+
+        Fornecedor fornecedorAtualizado = new Fornecedor(nome, telefone, cnpj);
+
+        repositorioFornecedor.EditarRegistro(id, fornecedorAtualizado);
+
+        ViewBag.Contexto = "Fornecedores";
+        ViewBag.Tipo = "fornecedor";
+        ViewBag.Mensagem = $"O registro \"{fornecedorAtualizado.Nome}\" foi editado com sucesso!";
+
+        return View("Notificacao");
+    }
+
+    [HttpGet("excluir/{id:int}")]
+    public IActionResult FormularioExcluir(int id) // exemplo sem [FromRoute], nao precisa pois refere ao id da rota "{excluir/id:int}"
+    {
+        ContextoDados contexto = new ContextoDados(true);
+        IRepositorioFornecedor repositorioFornecedor = new RepositorioFornecedorEmArquivo(contexto);
+
+        ViewBag.Fornecedor = repositorioFornecedor.SelecionarRegistroPorId(id);
+
+        return View("Excluir");
+    }
+
+    [HttpPost("excluir/{id:int}")]
+    public IActionResult Excluir(int id) // exemplo sem [FromRoute], nao precisa pois refere ao id da rota "{excluir/id:int}"
+    {
+        ContextoDados contexto = new ContextoDados(true);
+        IRepositorioFornecedor repositorioFornecedor = new RepositorioFornecedorEmArquivo(contexto);
+
+        repositorioFornecedor.ExcluirRegistro(id);
+
+        ViewBag.Contexto = "Fornecedores";
+        ViewBag.Tipo = "fornecedor";
+        ViewBag.Mensagem = $"Registro excluído com sucesso!";
+
+        return View("Notificacao");
+    }
+}
