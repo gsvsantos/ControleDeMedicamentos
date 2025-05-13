6f0b6870ad49fb86e2b806a3a7853146732d1b2e
gsvsantos <gustavocnsantos02@gmail.com>
2025-05-13 18:52:25 -0300

    feat: Criado `FuncionarioExtensions.cs` e implementado métodos de extensão.

---
+++ ControleDeMedicamentos.ConsoleApp/Extensions/FuncionarioExtensions.cs
@@ -0,0 +1,17 @@
+﻿using ControleDeMedicamentos.ConsoleApp.Models;
+using ControleDeMedicamentos.ConsoleApp.ModuloFuncionario;
+
+namespace ControleDeMedicamentos.ConsoleApp.Extensions;
+
+public static class FuncionarioExtensions
+{
+    public static Funcionario ParaEntidade(this FormularioFuncionarioViewModel formularioVM)
+    {
+        return new(formularioVM.Nome!, formularioVM.Telefone!, formularioVM.CPF!);
+    }
+
+    public static DataFuncionarioViewModel ParaDetalhesVM(this Funcionario funcionario)
+    {
+        return new(funcionario.Id, funcionario.Nome!, funcionario.Telefone!, funcionario.CPF!);
+    }
+}
