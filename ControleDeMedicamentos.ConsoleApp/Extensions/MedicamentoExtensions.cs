cb70f7b78e12e4dea79bb91bc70dd5497dd9dfbc
gsvsantos <gustavocnsantos02@gmail.com>
2025-05-18 19:33:47 -0300

    refactor: Implementado ViewModels e Extensions de Paciente.

---
+++ ControleDeMedicamentos.ConsoleApp/Extensions/MedicamentoExtensions.cs
@@ -0,0 +1,36 @@
+﻿using ControleDeMedicamentos.ConsoleApp.Models;
+using ControleDeMedicamentos.ConsoleApp.ModuloFornecedor;
+using ControleDeMedicamentos.ConsoleApp.ModuloMedicamento;
+
+namespace ControleDeMedicamentos.ConsoleApp.Extensions;
+
+public static class MedicamentoExtensions
+{
+    public static Medicamento ParaEntidade(this FormularioMedicamentoViewModel formularioVM, List<Fornecedor> fornecedores)
+    {
+        Fornecedor fornecedorSelecionado = null!;
+
+        foreach (Fornecedor f in fornecedores)
+        {
+            if (f.Id == formularioVM.FornecedorId)
+                fornecedorSelecionado = f;
+        }
+
+        return new(
+            formularioVM.Nome!,
+            formularioVM.QtdEstoque,
+            formularioVM.Descricao!,
+            fornecedorSelecionado);
+    }
+
+    public static DataMedicamentoViewModel ParaDetalhesVM(this Medicamento medicamento)
+    {
+        return new(
+            medicamento.Id,
+            medicamento.Nome!,
+            medicamento.QtdEstoque,
+            medicamento.Descricao!,
+            medicamento.Status!,
+            medicamento.Fornecedor!.Nome!);
+    }
+}
---
+++ ControleDeMedicamentos.ConsoleApp/Models/MedicamentoViewModels.cs
@@ -0,0 +1,107 @@
+﻿using ControleDeMedicamentos.ConsoleApp.Extensions;
+using ControleDeMedicamentos.ConsoleApp.ModuloFornecedor;
+using ControleDeMedicamentos.ConsoleApp.ModuloMedicamento;
+
+namespace ControleDeMedicamentos.ConsoleApp.Models;
+
+public abstract class FormularioMedicamentoViewModel
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public int QtdEstoque { get; set; }
+    public string? Descricao { get; set; }
+    public string? Status { get; set; }
+    public string? NomeFornecedor { get; set; }
+    public int FornecedorId { get; set; }
+    public List<SelecionarFornecedorViewModel> FornecedoresDisponiveis { get; set; } = [];
+}
+
+public class SelecionarFornecedorViewModel
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+
+    public SelecionarFornecedorViewModel(int id, string nome)
+    {
+        Id = id;
+        Nome = nome;
+    }
+}
+
+public class CadastrarMedicamentoViewModel : FormularioMedicamentoViewModel
+{
+    public CadastrarMedicamentoViewModel() { }
+    public CadastrarMedicamentoViewModel(List<Fornecedor> fornecedores)
+    {
+        foreach (Fornecedor f in fornecedores)
+        {
+            SelecionarFornecedorViewModel selecionarVM = new(f.Id, f.Nome!);
+
+            FornecedoresDisponiveis.Add(selecionarVM);
+        }
+    }
+}
+
+public class VisualizarMedicamentosViewModel
+{
+    public List<DataMedicamentoViewModel> Registros { get; set; } = [];
+
+    public VisualizarMedicamentosViewModel(List<Medicamento> medicamentos)
+    {
+        foreach (Medicamento m in medicamentos)
+        {
+            DataMedicamentoViewModel dataVM = m.ParaDetalhesVM();
+
+            Registros.Add(dataVM);
+        }
+    }
+}
+
+public class EditarMedicamentoViewModel : FormularioMedicamentoViewModel
+{
+    public EditarMedicamentoViewModel() { }
+    public EditarMedicamentoViewModel(int id, string nome, int qtdEstoque, string descricao, string status, List<Fornecedor> fornecedores)
+    {
+        Id = id;
+        Nome = nome;
+        QtdEstoque = qtdEstoque;
+        Descricao = descricao;
+        Status = status;
+
+        foreach (Fornecedor f in fornecedores)
+        {
+            SelecionarFornecedorViewModel selecionarVM = new(f.Id, f.Nome!);
+
+            FornecedoresDisponiveis.Add(selecionarVM);
+        }
+    }
+}
+
+public class ExcluirMedicamentoViewModel : FormularioMedicamentoViewModel
+{
+    public ExcluirMedicamentoViewModel() { }
+    public ExcluirMedicamentoViewModel(int id, string nome)
+    {
+        Id = id;
+        Nome = nome;
+    }
+}
+
+public class DataMedicamentoViewModel : FormularioMedicamentoViewModel
+{
+
+    public DataMedicamentoViewModel(int id, string nome, int qtdEstoque, string descricao, string status, string nomeFornecedor)
+    {
+        Id = id;
+        Nome = nome;
+        QtdEstoque = qtdEstoque;
+        Descricao = descricao;
+        Status = status;
+        NomeFornecedor = nomeFornecedor;
+    }
+
+    public override string ToString()
+    {
+        return $"ID: {Id}, Fornecedor; {NomeFornecedor}, Nome: {Nome},  Quantidade em Estoque: {QtdEstoque}, Descrição: {Descricao}, Status: {Status}";
+    }
+}
